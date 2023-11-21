8b88bee0d60166bb7754f0e3e4338c4d4ad4a384
e255140 <e255140@edu.sbl.ch>
2023-11-21 16:11:20 +0100

    finished exercise 1 21.11.23

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ export function exercise01(args) {
   const result = []
 
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-    result.push(currentElement)
+   const currentElement = input[i]
+    if (currentElement === 'e' || currentElement === "E") {
+      result.push("")
+    }
+    else {
+      result.push(currentElement)
+    }
   }
   return result.join("")
 }
+
