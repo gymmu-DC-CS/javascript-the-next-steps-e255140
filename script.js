196e5e7b53cd0877883d4bd3c9026161db25a892
e255140 <e255140@edu.sbl.ch>
2023-12-07 15:43:50 +0100

    I tried to solve exercise 4 and I am on the path

--- script.js
+++ script.js
@@ -3,19 +3,51 @@ export function exercise01(args) {
   const result = []
 
   for (let i = 0; i < input.length; i++) {
-   const currentElement = input[i]
-    if (currentElement === 'e' || currentElement === "E") {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
       result.push("")
-    }
-    else {
+    } else {
       result.push(currentElement)
     }
   }
   return result.join("")
 }
-  
-  
+
 export function exercise02(args) {
   return args.toUpperCase()
 }
 
+export function exercise03(args) {
+  const input = args
+  const result = []
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
+}
+
+export function exercise04(args) {
+  const input = "Two Words"
+  const result = []
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "spaces") {
+      count = count + 1
+    }
+  }
+  return arr.filter((word) => word !== "").length
+}
+export function countWords(str) {
+  const arr = str.split(" ")
+
+  return arr.filter((word) => word !== "").length
+}
